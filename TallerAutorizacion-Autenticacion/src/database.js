1cffbcc26516694e01d2d18dc5ea9382b2d83aee
Miguel Bohorquez Lopez <dlsg1009@gmail.com>
2021-09-03 17:28:04 -0500

    Proyecto

--- TallerAutorizacion-Autenticacion/src/database.js
+++ TallerAutorizacion-Autenticacion/src/database.js
@@ -1,3 +1,8 @@
 import mongoose from 'mongoose'
 
-mongoose.connect("")
+mongoose.connect("mongodb+srv://ups:ups2021@cluster0.8cey7.mongodb.net/companydb?retryWrites=true&w=majority",{
+useNewUrlParser: true,
+useUnifiedTopology: true
+})
+    .then(db => console.log('Db is connected'))
+    .catch(error => console.log(error))
