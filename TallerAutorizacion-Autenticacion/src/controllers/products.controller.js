c49eb33bff2301a67606b6d8b85502d4dfa2af09
Miguel Bohorquez Lopez <dlsg1009@gmail.com>
2021-09-03 23:24:11 -0500

    Proyecto

--- TallerAutorizacion-Autenticacion/src/controllers/products.controller.js
+++ TallerAutorizacion-Autenticacion/src/controllers/products.controller.js
@@ -1,19 +1,32 @@
-export const createProduct = (req, res) => {
+import Product from '../models/Product'
 
+export const createProduct = async (req, res) => {
+    const {name, category, price, imgURL} = req.body
+
+    const newProduct = new Product({name, category, price, imgURL});
+    
+    const productSaved = await newProduct.save()
+
+    res.status(201).json(productSaved)
 }
 
-export const getProducts = (req, res) => {
-    res.json("get products")
+export const getProducts = async (req, res) => {
+    const products = await Product.find();
+    res.json(products)
 }
 
-export const getProductById = (req, res) => {
-
+export const getProductById = async (req, res) => {
+    const product = await Product.findById(req.params.productId);
+    res.status(200).json(product)
 }
 
-export const updateProductById = (req, res) => {
-
+export const updateProductById = async (req, res) => {
+    const updatedProduct = await Product.findByIdAndUpdate(req.params.productId, req.body, {
+        new: true
+    })
+    res.status(200).json(updatedProduct)
 }
 
-export const deleteProductById = (req, res) => {
+export const deleteProductById = async (req, res) => {
 
 }
